a85c97ea667598f8594a3053d1a938b3672eb55c
grahamd <devnull@localhost>
2005-03-18 09:29:05 +0000

    Documentation.

---
+++ examples/handler/publish-objects.py
@@ -0,0 +1,26 @@
+import vampire
+
+class _Object:
+
+  def __init__(self):
+    self.value1 = "value1"
+
+  def method1(self):
+    return "method1()"
+
+  def method2(self,req):
+    req.content_type = "text/plain"
+    req.send_http_header()
+    req.write("method2()")
+
+  def __call__(self):
+    return "__call__()"
+
+_object = _Object()
+
+handler = vampire.Publisher(_object)
+
+def handler_txt(req):
+  req.content_type = "text/plain"
+  req.send_http_header()
+  req.write("handler_txt()")
