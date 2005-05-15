fb05ea9e3075034015a98caaad07f25ef8f46c5c
grahamd <devnull@localhost>
2005-05-15 07:27:05 +0000

    PSP handler made more compatible with standard PSP handler.

--- examples/psp/_handler.py
+++ examples/psp/_handler.py
@@ -1,43 +1,138 @@
 from mod_python import apache
 
 import os
+import sys
 import vampire
 
-# Default handler for HTML.
+# We can't use the "import" statement for "psp" because
+# of strange problems in mod_python module loader.
+# Namely, if "import" is used and PythonHandler is
+# defined elsewhere as "mod_python.psp" and it gets
+# loaded before this code, the "psp" module can not then
+# be found. Instead use the mod_python module loader
+# itself as then it always works.
 
-def handler_html(req):
+#from mod_python import psp
+psp = apache.import_module("mod_python.psp")
 
-  # Check for existance of PSP source file.
+
+# Handler for serving up PSP files. The handler will
+# maintain compatibility with how standard PSP works,
+# except that the ".psp" extension isn't used. Depending
+# on how Vampire configuration is setup, either ".html"
+# extension is used, or REST style URL with no actual
+# extension.
+
+def handler(req):
+
+  # We only want to treat request as being a possible
+  # request for a PSP template file if there exists a
+  # ".psp" file.
 
   path = os.path.splitext(req.filename)[0] + ".psp"
 
-  if os.path.exists(path):
+  if not os.path.exists(path):
+    return apache.DECLINED
 
-    # Hand off request to PSP handler.
+  # PSP template files are always HTML files.
 
-    from mod_python import psp
+  req.content_type = "text/html"
 
-    req.content_type = "text/html"
+  # Trigger parsing of the PSP file. The file cache
+  # for PSP files is still being used here.
 
-    config = vampire.loadConfig(req,".vampire")
+  template = psp.PSP(req,filename=path)
 
-    settings = {}
+  code = template.code
 
-    for key,value in config.items("Settings"):
-      settings[key] = value
+  # Check whether the code is trying to make use of
+  # sessions. If it is, first look for session created
+  # externally and stored in the req object. If this
+  # doesn't exist only then create a session object.
 
-    if hasattr(req,"form"):
-      settings["form"] = req.form
+  session = None
 
-    template = psp.PSP(req,filename=path,vars=settings)
+  if "session" in code.co_names:
+    if hasattr(req,"session"):
+      session = req.session
+    else:
+      session = Session.Session(req)
 
-    template.run()
+  # Check whether the code is trying to make use of the
+  # form object. If it is, use the form object created
+  # by Vampire. Note that no steps are taken to push
+  # Vampire decoded form parameters into the execution
+  # environment, instead compatibility is kept with how
+  # PSP does things.
 
-    return apache.OK
+  form = None
 
-  return apache.DECLINED
+  if "form" in code.co_names:
+    vampire.processForm(req)
+    form = req.form
 
-# Default handler for PSP.
+  # Create PSP interface object for compatibility.
+
+  interface = psp.PSPInterface(req,template.filename,form)
+
+  # Build up the execution environment to be used.
+  # Defaults from Vampire configuration are also pushed
+  # into the environment so that these can be used. If
+  # user defined settings are placed into the "DEFAULT"
+  # section of configuration file, these will also be
+  # available.
+
+  environ = globals().copy() 
+
+  config = vampire.loadConfig(req,".vampire")
+  environ.update(config.defaults())
+
+  environ["req"] = req
+  environ["session"] = session
+  environ["form"] = form
+  environ["psp"] = interface
+
+  # Now execute the actual page to handle the request.
+
+  try:
+
+    try:
+      exec code in environ
+
+      req.flush()
+
+      # Always ensure that session object is saved.
+
+      if session is not None:
+	session.save()
+
+    except: 
+      et,ev,etb = sys.exc_info()
+
+      # Use error page to display details of error if
+      # an actual error page was supplied.
+
+      if interface.error_page:
+	template.error_page.run({"exception":(et,ev,etb)})
+
+      else:
+	raise et,ev,etb
+
+  finally:
+    if session is not None:
+      session.unlock()
+
+
+# Also link handler to that for ".html" requests. Which
+# is used and thus whether REST style URLs are used or
+# requests with a ".html" extension is dictated by how
+# settings are defined in the Vampire configuration
+# file.
+
+handler_html = handler                                                          
+
+
+# Block access to raw PSP files.
 
 def handler_psp(req):
   if os.path.exists(req.filename):
